c153d51574e489805956959e67b8b97db28f351d
ActuallyTheOwner <wellswolelouie@yahoo.com>
2025-10-05 19:25:34 -0500

    Improve intro

--- src/oak_speech.c
+++ src/oak_speech.c
@@ -1500,9 +1500,12 @@ static void Task_OakSpeech_HandleConfirmNameInput(u8 taskId)
     case MENU_B_PRESSED:
         PlaySE(SE_SELECT);
         if (sOakSpeechResources->hasPlayerBeenNamed == FALSE)
-            gTasks[taskId].func = Task_OakSpeech_FadeOutForPlayerNamingScreen;
+            gTasks[taskId].func = Task_OakSpeech_FadeOutOak;
         else
-            gTasks[taskId].func = Task_OakSpeech_RepeatNameQuestion;
+            {
+            sOakSpeechResources->hasPlayerBeenNamed = FALSE;
+            gTasks[taskId].func = Task_OakSpeech_FadeOutOak;
+            }
         break;
     }
 }
